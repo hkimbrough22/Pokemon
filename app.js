ef13443e30ee521ef79b369d31a5e826e691e57a
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-07-01 12:32:29 -0700

    Consolidated JS into one file

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 console.log('Hello World!');
+function getUsername (){
+    let userName = prompt('Please enter your name');
+    document.write("Hello " + userName + ", ");
+}
+getUsername();
 
+function getPMpref(){
+    let likePM = prompt("Do you like Pokemon?");
+    if(likePM == "yes"){
+        alert("I like Pokemon too!");
+    }
+    else (alert("I'm sorry to hear that! Maybe try Digimon, you square!"));
+}
+getPMpref();
+function getTOD (){
+    let day = new Date ();
+    let time = day.getHours ();
+    console.log(time);
+    if (time <= 10){
+        return " Good Morning! Welcome!";
+    } else if (time >10 && time < 14){
+        return " Good Afternoon! Welcome!";
+    } else {
+        return " Good Evening! Welcome!";
+    }
+}
+let element = getTOD();
+document.write(element);
 
-//let userName = 'Haustin';
-//console.log(userName)
-
-
-let userName = prompt('Please enter your name');
-document.write("Hello " + userName);
--- app2.js
+++
@@ -1,5 +0,0 @@
-let likePM = prompt("Do you like Pokemon?");
-if(likePM == "yes"){
-    alert("I like Pokemon too!");
-}
-else (document.write("I'm sorry to hear that!"));
